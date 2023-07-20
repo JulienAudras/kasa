96a9aa342c8a6818c945c53b3e3790eef942c848
Julien Audras <audras.julien@gmail.com>
2023-07-20 14:25:53 +0200

    error page complete

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,17 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
-
+import { Link } from "react-router-dom";
 
 
 function Error() {
   return (
     <div>
       <Header />
+      <div className="errorPage">
+        <h1 className="errorPage__404">404</h1>
+        <p className="errorPage__content">Oups! La page que vous demandez n'existe pas.</p>
+        <Link to="/" className="errorPage__link">Retourner sur la page d'accueil</Link>
+      </div>
       <Footer />
     </div>
   )
