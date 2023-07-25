faeb2da5a68004823dc1a36557d2d2ef02bb188a
Julien Audras <audras.julien@gmail.com>
2023-07-25 09:29:17 +0200

    Acordeon + page about

---
+++ src/components/AccordeonItem.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from "react";
+import arrow from "../assets/arrow_up-24px.png";
+import arrow_mobile from "../assets/arrow_up_mobile.png"
+
+const AccordeonItem = ({ title, content, className }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  const toggleAccordeon = () => {
+    setIsOpen(!isOpen);
+  }
+
+  useEffect(() => {
+    const contentHeight = contentRef.current.scrollHeight;
+    contentRef.current.style.height = isOpen ? `${contentHeight}px` : "0px";
+  }, [isOpen]);
+
+
+  return (
+    <div className={`accordeon-item ${className}`}>
+      <div className="accordeon-item__title">
+        <span className="accordeon-item__title--text">{title}</span>
+        <img
+          className={`accordeon-item__title--arrow ${isOpen ? "open" : "closed"}`}
+          src={arrow}
+          alt="Flèche"
+          onClick={toggleAccordeon}
+        />
+        <img
+          className={`accordeon-item__title--arrow_mobile ${isOpen ? "open" : "closed"}`}
+          src={arrow_mobile}
+          alt="Flèche"
+          onClick={toggleAccordeon}
+        />
+      </div>
+      <div
+        className={`accordeon-item__content`}
+        ref={contentRef}
+        style={{
+        height: isOpen ? contentRef.current.scrollHeight + "px" : "0px",
+        transition: "height 0.5s ease"
+        }}
+      >
+        <div className="accordeon-item__content--text">
+          {content}
+        </div>
+        </div>
+    </div>
+  );
+};
+
+export default AccordeonItem;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,52 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import AccordeonItem from "../components/AccordeonItem.jsx";
+import MountainPhoto from "../assets/Mountain.jpeg";
 
+const aboutDatas=
+[
+  {
+    title:"Fiabilité",
+    content:"Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+  },
+  {
+    title:"Respect",
+    content:"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+  },
+  {
+    title:"Service",
+    content:"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+  },
+  {
+    title:"Sécurité",
+    content:"La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+  }
+]
 
 
 function About() {
   return (
     <div>
       <Header />
+      <div className="about-container">
+        <div className="about-photo">
+          <img 
+            className="about-photo__image"
+            src={MountainPhoto}
+            alt="paysage montagneu"
+            />
+        </div>
+        <div className="accordeons-container">
+        {aboutDatas.map((item, index) => (
+          <AccordeonItem
+            key={index}
+            title={item.title}
+            content={item.content}
+            className={`accordeon-item-about about-${index+1}`}
+          />
+        ))}
+        </div>
+      </div>
       <Footer />
     </div>
   )
