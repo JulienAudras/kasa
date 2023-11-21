7ea6327c39adc75bf93b3918a2306c766eefd783
Julien Audras <audras.julien@gmail.com>
2023-11-21 11:55:12 +0100

    Comit final

--- src/components/BannerImage.jsx
+++ src/components/BannerImage.jsx
@@ -2,11 +2,12 @@ function BannerImage(props) {
   return (
     <div className={props.class}>
         <img 
-            className= {props.photoClass}
+            className={props.photoClass}
             src={props.bannerPhoto}
-            alt="Bannière du site"
+            alt={props.title || "Bannière du site"} 
+            
         />
-        <h1 className="bannerContainer__title">{props.title}</h1>
+        {props.title && <h1 className="bannerContainer__title">{props.title}</h1>}   
         </div>
   )
 }
